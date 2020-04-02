5d7c79f567ba3c8931064ff48f8dd1a0601194e2
Caroline Zaini <zainicaroline@gmail.com>
2020-04-02 15:25:00 +0200

    add code in viewController

--- Facebook_Autolayout/Facebook_Autolayout/ViewController.swift
+++ Facebook_Autolayout/Facebook_Autolayout/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var profileIV: UIImageView!
+    @IBOutlet weak var storyBtn: UIButton!
+    @IBOutlet var squareBtn: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        storyBtn.layer.cornerRadius = 15
+        
+        for button in squareBtn {
+            /// pour chaque bouton de la collection on lui mettra un corner radius de 15
+            button.layer.cornerRadius = 15
+        }
+        
+        profileIV.layer.cornerRadius = 75
+        /// systemBackground : si on a un fond noir, ce sera blanc et si le fond est blanc ce sera noir
+        profileIV.layer.borderColor = UIColor.systemBackground.cgColor
+        profileIV.layer.borderWidth = 4
     }
-
 
 }
 
