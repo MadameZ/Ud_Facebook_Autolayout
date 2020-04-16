5b2c1c87a13678fda01db35cc91269e2ed212c52
Caroline Zaini <zainicaroline@gmail.com>
2020-04-16 19:35:09 +0200

    add Gesture and touches

--- Facebook_Autolayout/Facebook_Autolayout/ViewController.swift
+++ Facebook_Autolayout/Facebook_Autolayout/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var profileIV: UIImageView!
     @IBOutlet weak var storyBtn: UIButton!
     @IBOutlet var squareBtn: [UIButton]!
+    @IBOutlet weak var coverIV: UIImageView!
+    
+    var center: CGPoint?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +31,63 @@ class ViewController: UIViewController {
         /// systemBackground : si on a un fond noir, ce sera blanc et si le fond est blanc ce sera noir
         profileIV.layer.borderColor = UIColor.systemBackground.cgColor
         profileIV.layer.borderWidth = 4
+        
+        coverIV.isUserInteractionEnabled = true
+        coverIV.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(gesture)))
+    }
+    
+  
+    
+    /// Gesture
+    
+    @objc func gesture() {
+        UIView.animate(withDuration: 1) {
+            if self.profileIV.layer.cornerRadius == 0 {
+                self.profileIV.layer.cornerRadius = 75
+            } else {
+                self.profileIV.layer.cornerRadius = 0
+            }
+            
+        }
     }
 
+    /// Touches
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let first = touches.first {
+            if first.view == profileIV {
+                center = profileIV.center
+            }
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let first = touches.first {
+            if first.view == profileIV {
+                profileIV.center = first.preciseLocation(in: self.view)
+            }
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let first = touches.first {
+            if first.view == profileIV {
+                if center != nil {
+                     profileIV.center = center!
+                }
+               
+            }
+        }
+    }
+    
+   
+    /// Simple
+    @IBAction func storyButtonPressed(_ sender: Any) {
+        storyBtn.backgroundColor = UIColor.systemTeal
+    }
+    
+    /// Multiple
+    @IBAction func squareButtons(_ sender: UIButton) {
+        coverIV.alpha = CGFloat(sender.tag)
+    }
 }
 
